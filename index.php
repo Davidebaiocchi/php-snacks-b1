30c2311a57a3c35af51f44ccd28ab9afd4a48870
davide <davide.baiocchi97@gmail.com>
2021-07-26 15:42:40 +0200

    array

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+$partite = [
+
+    [
+        'casa' => 'Lakers',
+        'ospite' => 'Bulls',
+        'puntiCasa' => '55',
+        'puntiOspiti' => '60',
+    ],
+    [
+        'casa' => 'Jazz',
+        'ospite' => 'Knicks',
+        'puntiCasa' => '55',
+        'puntiOspiti' => '89',
+        
+    ],
+    [
+        'casa' => 'Cavaliers',
+        'ospite' => 'Celtic',
+        'puntiCasa' => '32',
+        'puntiOspiti' => '67',
+    ],
+
+]
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Partite NBA</title>
+</head>
+<body>
+
+    <h2> Snack 1 </h2>
+    
+</body>
+</html>

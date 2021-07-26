48cdaa344035d340f2fffbc8a4d1a9c31a987727
davide <davide.baiocchi97@gmail.com>
2021-07-26 15:52:29 +0200

    Fine primo snack

--- Snack1/index.php
+++ Snack1/index.php
@@ -6,27 +6,27 @@ $partite = [
         'casa' => 'Lakers',
         'ospite' => 'Bulls',
         'puntiCasa' => '55',
-        'puntiOspiti' => '60',
+        'puntiOspite' => '60',
     ],
     [
         'casa' => 'Jazz',
         'ospite' => 'Knicks',
         'puntiCasa' => '55',
-        'puntiOspiti' => '89',
+        'puntiOspite' => '89',
         
     ],
     [
         'casa' => 'Cavaliers',
         'ospite' => 'Celtic',
         'puntiCasa' => '32',
-        'puntiOspiti' => '67',
+        'puntiOspite' => '67',
     ],
 
-]
+];
+
+$lunghezza = count($partite);
 
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -39,6 +39,15 @@ $partite = [
 <body>
 
     <h2> Snack 1 </h2>
-    
+    <?php for($i = 0; $i < $lunghezza; $i++){ ?>
+    <div class="">
+        <ul>
+            <li>
+                <?php echo $partite[$i]['casa'];?> - <?php echo $partite[$i]['ospite'];?> | <?php echo $partite[$i]['puntiCasa'];?> - <?php echo $partite[$i]['puntiOspite'];?>
+            </li>
+        </ul>
+    </div>
+    <?php } ?>
+
 </body>
 </html>
